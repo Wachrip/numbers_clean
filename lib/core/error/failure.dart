bbd43cea23edf23a42b7e16833552a3790819fef
wach <svardmitriy@gmail.com>
2023-11-23 15:29:11 +0600

    folder structure added

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  // Failure([List properties = const <dynamic>[]]) : super();
+
+  @override
+  List<Object?> get props => [];
+  // List get props => [properties];
+}
